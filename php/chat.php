96e3626a02a5f00488e5028c1833d40e1c2f7ace
Vincent <vincent.conta@gmail.com>
2019-05-06 10:11:18 +0200

    add php

---
+++ php/chat.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'bdd.php';
+
+if(array_key_exists("userMessage", $_POST) 
+&& !empty($_POST["userMessage"])
+{
+
+	$message = $_POST['userMessage'];
+    $discutId = $_POST["discut_id"];
+    $userId = $_POST["user_id"]; 
+
+	$requete=$bdd->prepare("INSERT INTO Message (text, discut_id, user_id) VALUES (? , ?, ?)");
+	$requete->execute([$message, $discutId, $userId]);
+	$messages = $requete->fetch();
+}
+
+echo json_encode($messages);
